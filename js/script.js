b58cd4ec929620bd41b15bf935895982552d15de
YoonA <depo89055@gmail.com>
2022-06-12 16:16:08 +0900

    .
    .

--- js/script.js
+++ js/script.js
@@ -9,7 +9,22 @@ window.onload = function(){
         showActiveTooltip: true,
         menu: '#menu'
     });
-    
+
+    var prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+
+    }
+
+
+    // ------------------- text 삽입 -------------------
     document.getElementById("easyVegan").innerText = "쉬운 비건, 쉬운 실천!";
     
     document.querySelector(".logoInfo1").innerText = "비건플래너는 둥근 형태와 Sandoll 티비스켓 서체를 활용하여 친근감을 나타낸다. 미소 짓는 입과 이파리를 응용한 형태는 즐겁게 비건을 실천하는 이용자를 표현한다. 또한 새싹은 새로이 도전하는 사람들을 상징한다.";
@@ -46,26 +61,3 @@ window.onload = function(){
     //     }
     // }
 }
-
-//^ CORS 허용
-// res.setHeader('Access-Control-Allow-origin', 'https://www.svgrepo.com/');
-// res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-// // 헤더에 작성된 출처만 브라우저가 리소스를 접근할 수 있도록 허용함.
-// Access-Control-Allow-Origin: <https://www.svgrepo.com/>
-
-// // 리소스 접근을 허용하는 HTTP 메서드를 지정해 주는 헤더
-// Access-Control-Request-Methods: GET, POST, PUT, DELETE
-
-// // 서버에서 응답 헤더에 추가해 줘야 브라우저의 자바스크립트에서 헤더에 접근 허용
-// Access-Control-Expose-Headers: Authorization 
-
-// // preflight 요청 결과를 캐시 할 수 있는 시간을 나타냄.
-// // 60초 동안 preflight 요청을 캐시하는 설정으로, 첫 요청 이후 60초 동안은 OPTIONS 메소드를 사용하는 예비 요청을 보내지 않는다.
-// Access-Control-Max-Age: 60 
-
-// // 자바스크립트 요청에서 credentials가 include일 때 요청에 대한 응답을 할 수 있는지를 나타낸다
-// Access-Control-Allow-Credentials: true
-
-
-
