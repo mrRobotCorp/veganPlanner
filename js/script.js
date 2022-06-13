3eee0e08c0b234e004a27e2360f0a68156ded6ad
YoonA <depo89055@gmail.com>
2022-06-13 14:52:27 +0900

    .
    .

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
 window.onload = function(){
+
     // ------------------ loading hide ------------------
     document.querySelector(".loading").style.visibility = "hidden";
 
@@ -11,7 +11,124 @@ window.onload = function(){
         menu: '#menu'
     });
 
-    // ------------------- text -------------------
+    // --------------------- todo list --------------------
+    var addButton = document.getElementById('addButton');
+    var addInput = document.getElementById('itemInput');
+    var todoList = document.getElementById('todoList');
+    var listArray = [];
+
+    function listItemObj(content, status) {
+        this.content = '';
+        this.status = 'incomplete';
+    }
+    var changeToComp = function(){
+        var parent = this.parentElement;
+        parent.className = 'uncompleted well';
+        this.removeEventListener('click',changeToComp);
+        this.addEventListener('click',changeToInComp);
+        changeListArray(parent.firstChild.innerText,'complete');
+    }
+
+    var changeToInComp = function(){
+        var parent = this.parentElement;
+        parent.className = 'completed well';
+        this.removeEventListener('click',changeToInComp);
+        this.addEventListener('click',changeToComp);
+
+        changeListArray(parent.firstChild.innerText,'incomplete');
+    }
+
+    var removeItem = function(){
+        var parent = this.parentElement.parentElement;
+        parent.removeChild(this.parentElement);
+
+        var data = this.parentElement.firstChild.innerText;
+
+        for(var i=0; i < listArray.length; i++){
+            if(listArray[i].content == data){
+                listArray.splice(i,1);
+                refreshLocal();
+                break;
+            }
+        }
+    }
+
+    var changeListArray = function(data,status){
+
+        for(var i=0; i < listArray.length; i++){
+            if(listArray[i].content == data){
+                listArray[i].status = status;
+                refreshLocal();
+                break;
+            }
+        }
+    }
+
+    var createItemDom = function(text,status){
+        var listItem = document.createElement('li');
+        var itemLabel = document.createElement('label');
+        var itemCompBtn = document.createElement('button');
+        var itemIncompBtn = document.createElement('button');
+
+        listItem.className = (status == 'incomplete')?'completed well':'uncompleted well';
+
+        itemLabel.innerText = text;
+        itemCompBtn.className = 'btn btn-success';
+        if(status == 'incomplete'){
+            itemCompBtn.addEventListener('click',changeToComp);
+        }else{
+            itemCompBtn.addEventListener('click',changeToInComp);
+        }
+
+        itemIncompBtn.className = 'btn btn-danger';
+        itemIncompBtn.addEventListener('click',removeItem);
+        listItem.appendChild(itemLabel);
+        listItem.appendChild(itemCompBtn);
+        listItem.appendChild(itemIncompBtn);
+
+        return listItem;
+    }
+
+    var refreshLocal = function(){
+        var todos = listArray;
+        localStorage.removeItem('todoList');
+        localStorage.setItem('todoList', JSON.stringify(todos));
+    }
+
+    var addToList = function(){
+        var newItem = new listItemObj();
+        newItem.content = addInput.value;
+        listArray.push(newItem);
+        refreshLocal();
+
+        var item = createItemDom(addInput.value,'incomplete');
+        todoList.appendChild(item);
+        addInput.value = '';
+    }
+
+    var clearList = function(){
+        listArray = [];
+        localStorage.removeItem('todoList');
+        todoList.innerHTML = '';
+    }
+
+    var list = localStorage.getItem('todoList');
+
+    if (list != null) {
+        todos = JSON.parse(list);
+        listArray = todos;
+
+        for(var i=0; i<listArray.length;i++){
+            var data = listArray[i].content;
+            var item = createItemDom(data,listArray[i].status);
+            todoList.appendChild(item);
+        }
+    }
+
+    addButton.addEventListener('click', addToList);
+    clearButton.addEventListener('click', clearList);
+
+    // ------------------- innerText ----------------------
     document.getElementById("easyVegan").innerText = "쉬운 비건, 쉬운 실천!";
     
     document.querySelector(".logoInfo1").innerText = "비건플래너는 둥근 형태와 Sandoll 티비스켓 서체를 활용하여 친근감을 나타낸다. 미소 짓는 입과 이파리를 응용한 형태는 즐겁게 비건을 실천하는 이용자를 표현한다. 또한 새싹은 새로이 도전하는 사람들을 상징한다.";
